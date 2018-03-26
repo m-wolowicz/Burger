889791d11810ec48acea60a8f57f6f153cfcea09
Melissa Wolowicz <mwolowicz@MWolowicz.local>
2018-03-26 09:59:12 -0400

    Deploying to Heroku"

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,35 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+			host: "localhost",
+			user: "root",
+			password: "",
+			database: "burgers_db"
+		});
+};
 
-// Make connection.
-connection.connect(function(err) {
-	if (err) {
-		console.error("There was an error connecting to the MySQL Database: " + err.stack);
-		return;
-	}
-	console.log("You have successfully connected to the MySQL Database as id: " + connection.threadId);
-});
+
+// var connection = mysql.createConnection({
+// 	port: 3306,
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "",
+// 	database: "burgers_db"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+// 	if (err) {
+// 		console.error("There was an error connecting to the MySQL Database: " + err.stack);
+// 		return;
+// 	}
+// 	console.log("You have successfully connected to the MySQL Database as id: " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
